09fc7f1ba322f7182942f8d1e94b635c89c82e00
Edmario Oliveira <barrigacraft@hotmail.com>
2024-02-22 00:29:06 -0300

    feat/middleware

--- src/Router.php
+++ src/Router.php
@@ -17,10 +17,20 @@ class Router
         return $this;
     }
 
+    public function middleware(String $middleware, $httpStatusCode)
+    {
+        $reflectionClass = new ReflectionClass($middleware);
+        $middlewareInstance = $reflectionClass->newInstance();
+        if (!$middlewareInstance->handle()) {
+            http_response_code($httpStatusCode);
+            exit;    
+        }
+        return $this;
+    }
+    
     private function run()
     {
         $acessedRoute = $this->getAccessedRoute();
-        
         foreach($this->controllers as $controller) {
             $reflectionController = new ReflectionClass($controller);
             foreach($reflectionController->getMethods() as $method) {
@@ -47,7 +57,20 @@ class Router
         global $argv;
         $route = $this->isCli() ? $argv[2] : explode("?", $_SERVER['REQUEST_URI'])[0];
         $method = $this->isCli() ? $argv[1] : explode("?", $_SERVER['REQUEST_METHOD'])[0];
-
         return  new Route($route, $method);
     }
+
+    // private function attributeMatch($attributeRoute, $accessedRoute) : Mixed
+    // {
+    //     $routeInstance = $route->newInstance();
+        
+    //     if($routeInstance->isMatch($acessedRoute)) {
+    //         return (new Controller($controller))->runMethod(
+    //             $method->getName(), 
+    //             $routeInstance->getDynamicParameters($acessedRoute)
+    //         );
+    //     }
+
+    //     return true;
+    // }
 }
